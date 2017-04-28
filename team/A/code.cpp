2cd9246f9187c13e69b8008cae3de6c932f92bc3
Nikita Sychev <root@nsychev.ru>
2017-04-28 20:23:23 +0500

    team/A solution

---
+++ team/A/code.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<long long> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    sort(a.begin(), a.end());
+    int left = 0;
+    long long sum = 0, maxsum = 0;
+    for (int right = 0; right < n; ++right) {
+        sum += a[right];
+        while (right - left > 1 && a[left] + a[left + 1] < a[right]) {
+            sum -= a[left];
+            ++left;
+        }
+        if (sum > maxsum)
+            maxsum = sum;
+    }
+    cout << maxsum;
+    return 0;
+}
